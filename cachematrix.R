ad87126db0643e1ba98cb62d4039aa463ca9dc95
nikhila-arkalgud <nikhilaprasad@gmail.com>
2014-12-21 12:23:48 -0800

    made a few changes to the comment lines

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## There are two key functions here.
-## function makeCacheMatrix creates a special matrix with its inverse precalculated
+## There are key functions here.
+## function makeCacheMatrix creates a special "matrix" object which caches its inverse
 ## function cacheSolve returns the inverse of the matrix, if in cache it uses the cached value
-## if not calculates the inverse and stores it using the makeCacheMatrix function
+## if not calculates the inverse and stores it in cache
 
 ## Write a short comment describing this function
-## Creating a special matrix object with its inverse precalculated
+## Creating a special matrix object which caches its inverse
 makeCacheMatrix <- function(x = matrix()) {
         mInv <- NULL
         set <- function(y){
@@ -18,13 +18,13 @@ makeCacheMatrix <- function(x = matrix()) {
         setSolve <- function(solve) mInv <<- solve(x)
         getSolve <- function() mInv
         list(set = set, get = get, setSolve = setSolve, getSolve = getSolve)
-
+        
 }
 
 
 ## Write a short comment describing this function
-## THis function returns the inverse of the matrix, if the value is precalculated
-## it is retrieved from cache, else calculated and stored in the special matrix object
+## THis function returns the inverse of the matrix, if the value is in cache
+## it is retrieved from cache, else calculated and stored in cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         ## mInv will be the inverse of the matrix 'x' here
